0b3ffb2bede9c804c044fdb25cf52c9b0be8a75c
Glib13 <quldi410@gmail.com>
2022-01-08 02:01:50 +0200

    feat: done partly main menu

---
+++ mainmenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _ready():
+	$MarginContainer/VBoxContainer/startgame.grab_focus()
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		get_tree().quit()
+
+func _on_startgame_pressed():
+	get_tree().change_scene("res://level1/level1.tscn")
+
+
+func _on_additionalinfo_pressed():
+	pass # Replace with function body.
+
+
+func _on_qiutgame_pressed():
+	get_tree().quit()
