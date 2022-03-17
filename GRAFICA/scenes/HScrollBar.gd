2784f2892940ecdf01db274734a5313f3968f854
Glib13 <quldi410@gmail.com>
2022-03-17 14:38:23 +0200

    feat: better looking screen

---
+++ GRAFICA/scenes/HScrollBar.gd
@@ -0,0 +1,8 @@
+extends HScrollBar
+
+func _ready():
+	pass
+
+
+func _on_HScrollBar_changed():
+	print(value)
--- task.gd
+++ task.gd
@@ -3,21 +3,24 @@ func _ready():
 	pass
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("trans"):
-		$ColorRect/RichTextLabel.set_text(str($ColorRect/StaticLine.angle))
-		$transportir/Transportir.visible = false
+		$ColorRect2/ColorRect/RichTextLabel.set_text("Angle is: " + str($ColorRect2/ColorRect/StaticLine.angle))
+		$ColorRect2/transportir/Transportir.visible = false
 	if  body.is_in_group("Line"):
-		$lineBody.visible = false
-		$ColorRect/StaticLine/answerLine.drawing()
+		$ColorRect2/lineBody.visible = false
+		$ColorRect2/ColorRect/StaticLine/answerLine.drawing()
 #		$InputAngle.visible = true
-		$Container.visible = true
+		$ColorRect2/Container.visible = true
 #		if $InputAngle/MarginContainer/VBoxContainer/PanelContainer2/LineEdit.text == $ColorRect/StaticLine.angle / 2:
 #			get_tree().change_scene("res://levels/level1.tscn")
 			
 
 
 func _on_Button_pressed():
-	var Text = $Container/InputAngle/MarginContainer/VBoxContainer/PanelContainer2/LineEdit.text
-	if $ColorRect/StaticLine.angle / 2 == int(Text):
+#	var Text = $ColorRect2/Container/InputAngle/MarginContainer/VBoxContainer/PanelContainer2/LineEdit.text
+#	if $ColorRect2/ColorRect/StaticLine.angle / 2 == int(Text):
+#		get_tree().change_scene("res://levels/level1.tscn")
+#		$ColorRect2/transportir/Transportir.visible = false
+	var Text = $ColorRect2/Container/HScrollBar.value
+	if $ColorRect2/ColorRect/StaticLine.angle / 2 == int(Text):
 		get_tree().change_scene("res://levels/level1.tscn")
-		$transportir/Transportir.visible = false
-
+		$ColorRect2/transportir/Transportir.visible = false
